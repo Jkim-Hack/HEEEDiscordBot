72e3c2f7aaf7f348e7d3dc3d7358e7a9aa8f773a
John Kim <johnkim1108@gmail.com>
2018-09-20 22:48:10 -0400

    g

--- src/main/java/com/hee/heediscordbot/botEvents.java
+++ src/main/java/com/hee/heediscordbot/botEvents.java
@@ -9,6 +9,10 @@ public class botEvents {
     public void onMessageRecieved(MessageReceivedEvent event){
         if(event.getMessage().getContent().startsWith(App.PREFIX + "test"))
             App.sendMessage(event.getChannel(), "Wisone is GEIIIII");
+        if(event.getMessage().getContent().startsWith(App.PREFIX + "straights"))
+            App.sendMessage(event.getChannel(),  event.getGuild().getUsersByName("Huge-Dick Horny Hitler").
+                    get(0).getNicknameForGuild(event.getGuild())+" & " + event.getGuild().getUsersByName("Jizz Joey").
+                    get(0).getNicknameForGuild(event.getGuild()) +" is and always will be straight");
     }
 
 }
